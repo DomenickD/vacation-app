453a9779e3c1e5ea32d6db6541c96f13120c09bf
DomenickD <deothe1@gmail.com>
2022-06-19 12:42:32 -0400

    test 2

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res, next) => {
+    console.log('test');
+});
+
+app.listen(PORT, () => {
+    console.log('Port is running on 8080.')
+});
